8e58deba837faad4a26094400853a0ae5f079959
Andrew Kemm <andrewkemm@gmail.com>
2024-11-24 17:25:59 +1100

    Ratatui boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::io;
+use crossterm::event::{DisableMouseCapture, EnableMouseCapture};
+use crossterm::execute;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen};
+use ratatui::backend::{Backend, CrosstermBackend};
+use ratatui::Terminal;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // startup
+    enable_raw_mode()?;
+    let mut stderr = io::stderr();
+    execute!(stderr, EnterAlternateScreen, EnableMouseCapture)?;
+
+    let backend = CrosstermBackend::new(stderr);
+    let mut terminal = Terminal::new(backend)?;
+    let app_result = main_loop(&mut terminal);
+    
+    // teardown
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture,
+    )?;
+    terminal.show_cursor()?;
+    
+    if let Err(e) = app_result {
+        eprintln!("{e:?}");
+    }
+    
+    Ok(())
+}
+
+fn main_loop<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
+    Ok(())
 }
