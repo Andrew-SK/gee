b149b7e0a196ffdf7c43cd5e8628025793c6a3a1
Andrew Kemm <andrewkemm@gmail.com>
2024-11-25 14:18:41 +1100

    A bit of a mess about with command execution and formatting

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 use crossterm::event::{
-    DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEvent, KeyEventKind,
+    DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEventKind,
 };
 use crossterm::terminal::{
     disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
 };
 use crossterm::{event, execute};
 use ratatui::backend::{Backend, CrosstermBackend};
-use ratatui::layout::{Constraint, Layout};
+use ratatui::layout::{Alignment, Constraint, Layout};
 use ratatui::prelude::Direction;
 use ratatui::style::{Color, Style};
-use ratatui::text::Span;
-use ratatui::widgets::{Block, Borders, Padding, Paragraph};
+use ratatui::text::Line;
+use ratatui::widgets::{Block, Borders, List, ListItem};
 use ratatui::{Frame, Terminal};
-use std::env::split_paths;
 use std::error::Error;
 use std::io;
+use std::process::Command;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // startup
@@ -42,14 +42,29 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+pub struct App {
+    message: String,
+    status: Option<String>,
+}
+
 fn main_loop<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
+    let mut app = App {
+        message: String::from("Gee Whiz!"),
+        status: None,
+    };
     loop {
-        terminal.draw(|frame| render(frame))?;
+        terminal.draw(|frame| render(frame, &app.message))?;
 
         if let Event::Key(key) = event::read()? {
             if key.kind == KeyEventKind::Release {
                 continue;
             }
+
+            match key.code {
+                KeyCode::Char('q') => return Ok(()),
+                KeyCode::Char(' ') => run_cmd(&mut app),
+                _ => {}
+            };
 
             if key.code == KeyCode::Char('q') {
                 return Ok(());
@@ -58,22 +73,57 @@ fn main_loop<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
     }
 }
 
-fn render(frame: &mut Frame) {
+fn run_cmd(app: &mut App) {
+    let output = Command::new("ls").output().expect("ls failed to execute");
+
+    if output.status.success() {
+        app.message = String::from_utf8(output.stdout).unwrap()
+    } else {
+        app.message = format!("ERR: {:?}", output.stderr)
+    }
+}
+
+#[cfg(test)]
+mod test_run_cmd {
+    use std::process::Command;
+
+    #[test]
+    fn test_run_cmd() {
+        let output = Command::new("ls").output().expect("ls failed to execute");
+
+        assert!(output.status.success());
+        assert_ne!
+        (String::from_utf8(output.stdout), Ok(String::from("")));
+    }
+}
+
+fn render(frame: &mut Frame, message: &str) {
     let outer_block = Block::default().borders(Borders::ALL);
     frame.render_widget(outer_block, frame.area());
+
+    let text_parts: Vec<&str> = message.trim().split('\n').collect();
+
+    let mut lines = Vec::new();
+    for line in text_parts {
+        lines.push(ListItem::new(
+            Line::styled(line, Style::default().fg(Color::Yellow)).alignment(Alignment::Center),
+        ))
+    }
+    lines.push(ListItem::new(
+        Line::styled(
+            String::from("press 'q' to quit"),
+            Style::default().fg(Color::Yellow),
+        )
+        .alignment(Alignment::Center),
+    ));
+
+    let lines = List::new(lines).block(Block::default());
 
     let layout = Layout::default()
         .direction(Direction::Vertical)
         .margin(1)
-        .constraints([Constraint::Min(1), Constraint::Max(1)])
+        .constraints([Constraint::Min(1), Constraint::Max(lines.len() as u16)])
         .split(frame.area());
 
-    let text = Paragraph::new(Span::styled(
-        "Gee Whiz! press 'q' to quit",
-        Style::default().fg(Color::Yellow),
-    ))
-    .block(Block::default())
-    .centered();
-
-    frame.render_widget(text, layout[1]);
+    frame.render_widget(lines, layout[1]);
 }
